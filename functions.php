6563f41eb97dbcba39f7ac739ec360bf951420ae
Micah Gentzel <mgentzel@allebach.com>
2021-08-09 17:16:10 -0400

    footer animation on scroll

--- functions.php
+++ functions.php
@@ -59,7 +59,7 @@ function aca_theme_styles_and_scripts() {
     wp_enqueue_script( 'aca-theme-click-menu', get_template_directory_uri() . '/assets/js/click-menu.js', [], $theme_version );
     wp_enqueue_script( 'aca-theme-motion-text', get_template_directory_uri() . '/assets/js/motion-text.js', [], $theme_version );
     wp_enqueue_script( 'aca-theme-nav-toggle', get_template_directory_uri() . '/assets/js/main-nav.js', [], $theme_version );
-    wp_enqueue_script( 'aca-footer-scroll', get_template_directory_uri() . '/assets/js/footer.js', [], $theme_version );
+    // wp_enqueue_script( 'aca-footer-scroll', get_template_directory_uri() . '/assets/js/footer.js', [], $theme_version );
     wp_register_script('fontawesome-script', get_stylesheet_directory_uri() . '/inc/fontawesome/js/all.min.js', [], ACA_FONTAWESOME_VERSION);
     wp_enqueue_script('fontawesome-script');
     wp_register_script('aos-script-ext', get_stylesheet_directory_uri() . '/inc/aos/aos.js', [], $theme_version);
